873128408f759a8e26137bba2cd5273278359f6d
SmokedSalmon <jerrysu.jie@gmail.com>
2017-12-23 19:59:06 +0800

    fix page overflow

--- public/js/app_main.js
+++ public/js/app_main.js
@@ -344,7 +344,7 @@ var Util = function(t) {
         }))
     }
       , l = function() {
-        Modernizr.mq("(min-width: 360px)") && new Vivus("logo-anim",{
+        Modernizr.mq("(min-width: 1024px)") && new Vivus("logo-anim",{
             type: "delayed",
             delay: 48,
             duration: 60
@@ -361,7 +361,7 @@ var Util = function(t) {
               var e = window.baffle(titles[i], {  
                   characters: "█▓▒░█▓▒░█▓▒░<>/"
               });
-              Modernizr.mq("(min-width: 360px)") && setTimeout(function(e, initial) {
+              Modernizr.mq("(min-width: 1024px)") && setTimeout(function(e, initial) {
                   e.start().reveal(700 + initial, 800 + initial);
               }, 200, e, initial_time);
               i += 1;
