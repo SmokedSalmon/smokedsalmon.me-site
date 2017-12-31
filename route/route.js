5150a45a822c80724e522567b8f0525d57e2e716
SmokedSalmon <jerrysu.jie@gmail.com>
2017-12-31 16:30:42 +0800

    getting openid with code

--- route/route.js
+++ route/route.js
@@ -1,3 +1,6 @@
+var qs = require('querystring');
+var http = require('http');
+
 module.exports = function(app) {
   //WeChat verification for server ownership
   app.get('/wechat-m', function (req, res, next) {
@@ -6,15 +9,70 @@ module.exports = function(app) {
     res.send(echostr);
   });
   
+  // 1st step, request the 'code' ticket, related Wechat info are for testing only
   app.get('/wechat-auth', function (req, res, next) {
     var url_base = 'https://open.weixin.qq.com/connect/oauth2/authorize?',
         s_appid = 'appid=wx2a396a0704b080ea',
-        s_rURL = 'redirect_uri=' + encodeURIComponent('http://smokedsalmon.me'),
+        s_rURL = 'redirect_uri=' + encodeURIComponent('http://smokedsalmon.me/wechat-oid'),
         s_rType = 'response_type=code',
         s_scope = 'scope=snsapi_base',
         s_state = 'state=123#wechat_redirect',
         url = url_base + s_appid + '&' + s_rURL + '&' + + s_rType + '&' + s_scope + '&' + s_state;
         console.log(url);
     res.redirect(url);
+  });
+  
+  // 2nd step, use the code ticket t accquire openID
+  app.get('/wechat-oid', function (req, res, next) {
+    var query = req.query,
+        code = query.code,
+        url_base = 'https://api.weixin.qq.com',
+        path = '/sns/oauth2/access_token?'
+        s_appid = 'appid=wx2a396a0704b080ea',
+        s_secret = 'secret=093dfcd5c0631a6269feab22c3d81569',
+        s_code = '',
+        s_grantType = 'grant_type=authorization_code',
+        options = {}, request = {};
+    
+    if( !code || code === "" ) {
+      res.write('error getting code, ');
+      return;
+    } else {
+      //xhr = new XMLHttpRequest();
+      s_code = 'code=' + code;
+      path = path + s_appid + '&' + s_secret + '&' + s_code + '&' + s_grantType;
+      console.log('Requesting openID - ' + url_base + path);
+      //xhr.open('GET', url);
+      //xhr.onload = function() {
+      //  res.write(JSON.parse(xhr.responseText));
+      //}
+      options = {
+        hostname: url_base,
+        path: path,
+        method: 'GET',
+      };
+      
+      request = http.request(options, function(response){
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        response.setEncoding('utf8');
+        response.on('data', function(chunk){
+          console.log('BODY: ' + chunk);
+          res.write('BODY: ' + chunk);
+        });
+        
+        response.on('end', function(){
+          console.log('End of data in response');
+        });
+      });
+      
+      request.on('error', function(e) {
+        console.error('problem with request: ' + e.message);
+        res.write('problem with request: ' + e.message);
+        return;
+      });
+      
+      request.end();
+    }
   });
 };
